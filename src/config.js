57b85b73df5d10e2e79afe2ef962c55c6c14e130
DROPDOWN <pond2555555@gmail.com>
2020-03-19 21:23:29 +0700

    Lab09

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBKyqVWAO8j--laeLJJ7KDRsaKR4egxfNc",
+    authDomain: "todo-2541.firebaseapp.com",
+    databaseURL: "https://todo-2541.firebaseio.com",
+    projectId: "todo-2541",
+    storageBucket: "todo-2541.appspot.com",
+    messagingSenderId: "39661432075",
+    appId: "1:39661432075:web:b120b5e4dfd799c2e0060e",
+    measurementId: "G-G3L8VHH4N8"
+  };
+
+export default config
